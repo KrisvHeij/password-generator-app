138bc416db551488c08f5c04041f66fc595d1c35
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-11-22 19:22:14 +0100

    added function tp update character number

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const rangeSlider = document.getElementById("char-length");
+const charNumberEL = document.querySelector(".length-number");
 let charLength;
 
 
 function updateSlider () {
   const val = Number(rangeSlider.value);
-  const min = Number(rangeSlider.min);
+  // const min = Number(rangeSlider.min);
   const max = Number(rangeSlider.max);
-  const percent = ((val - min) / (max - min)) * 100;
-  console.log(percent);
+  const percent = (val / max) * 100;
+  // const percent = ((val - min) / (max - min)) * 100;
+  // console.log(percent);
   rangeSlider.style.background = `linear-gradient(to right, #a3ffae ${percent}%, #191820 ${percent}%)`;
+}
+
+function updateCharNumber () {
+  charNumberEL.textContent = charLength;
 }
 
 rangeSlider.addEventListener("input", () => {
   charLength = rangeSlider.value;
+  updateCharNumber();
   updateSlider();
 })
 
